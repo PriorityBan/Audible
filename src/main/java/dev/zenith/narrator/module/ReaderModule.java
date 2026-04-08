65a5691514830ebec2afb87b78dc74b14b9ba6d0
LeGrandOC <PrivatePerson1324@proton.me>
2026-04-08 15:19:19 +0100

    Update ReaderModule.java

--- src/main/java/dev/zenith/narrator/module/ReaderModule.java
+++ src/main/java/dev/zenith/narrator/module/ReaderModule.java
@@ -73,7 +73,7 @@ public class ReaderModule extends Module {
         loadFile();
         sent = 0;
         timer.reset();
-        System.out.println("[ReaderModule] Attempting to send: " + msg);
+        System.out.println(" Attempting to send a message  " ;
     }
 
     @Override
