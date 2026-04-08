20940a3de8856298f4df564b1f8bb3d31f26fbc4
LeGrandOC <PrivatePerson1324@proton.me>
2026-04-08 17:56:32 +0100

    Update NarratorCommand.java

--- src/main/java/dev/zenith/narrator/command/NarratorCommand.java
+++ src/main/java/dev/zenith/narrator/command/NarratorCommand.java
@@ -43,6 +43,7 @@ public class NarratorCommand extends Command {
 
                 c.getSource().getEmbed()
                     .title("Narrator " + toggleStrCaps(enabled));
+                return 1; // ✅ REQUIRED
             }));
     }
 
